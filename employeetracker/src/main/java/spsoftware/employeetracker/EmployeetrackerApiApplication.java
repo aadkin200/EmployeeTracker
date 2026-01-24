55667930a0dfabea9aac7dcbdcbf2f4d508aa090
aadkin200 <82674047+aadkin200@users.noreply.github.com>
2026-01-23 19:32:06 -0500

    added blank spring project | added blank database | added blank angular project

---
+++ employeetracker/src/main/java/spsoftware/employeetracker/EmployeetrackerApiApplication.java
@@ -0,0 +1,13 @@
+package spsoftware.employeetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeetrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeetrackerApiApplication.class, args);
+	}
+
+}
---
+++ employeetracker/src/test/java/spsoftware/employeetracker/EmployeetrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spsoftware.employeetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeetrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
