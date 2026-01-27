04efeda045a114a7e3cfff80b3e65928e9a172c3
aadkin200 <82674047+aadkin200@users.noreply.github.com>
2026-01-27 17:52:49 -0500

    created navbar

---
+++ employeetracker-ui/src/app/components/navbar/navbar.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.scss'],
+})
+export class Navbar {
+  constructor(
+    public auth: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
+
